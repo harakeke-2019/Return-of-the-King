e323881fca4d5cd54731fad94c4e528982da5f50
Benjamin Peter Little <benlittle53@gmail.com>
2019-01-25 11:54:27 +1300

    branch to seperate stretch and mvp

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,5 @@
 const express = require('express')
-
 const db = require('../db')
-
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -10,8 +8,52 @@ router.get('/', (req, res) => {
     .catch(displayErrors)
 
   function displayStoryName (info) {
-    console.log(info)
+    // console.log(info)
     res.render('index', {info})
+  }
+
+  function displayErrors (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+// update exisiting story
+router.get('/form/:id', (req, res) => {
+  console.log(req.params.id)
+  const id = Number(req.params.id)
+  db.updateStory(id)
+    .then(formatData)
+    .then(loadUpdatePage)
+    .catch(displayErrors)
+
+  function formatData(data) {
+    const info = {
+      name: data[0].name,
+      sentence: data[0].sentence
+    }
+    return info
+  }
+
+  function loadUpdatePage (info) {
+    res.render('form', info)
+  }
+
+  function displayErrors (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+// post new sentence and load story page
+router.post('/form/submit', (req, res) => {
+  const sentence = req.body.sentence
+  console.log(req.params.id)
+  db.addNewSentence()
+    .then(postStory)
+    .catch(displayErrors)
+
+  function postStory (story) {
+    // console.log(story)
+    res.render('')
   }
 
   function displayErrors (err) {
