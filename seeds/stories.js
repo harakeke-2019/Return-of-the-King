d052bc32cf59696d8f5082daa9c60902b534c366
Benjamin Peter Little <benlittle53@gmail.com>
2019-01-25 15:58:15 +1300

    basic mvp

--- seeds/stories.js
+++ seeds/stories.js
@@ -2,7 +2,7 @@ exports.seed = function (knex, Promise) {
   return knex('stories').del()
     .then(function () {
       return knex('stories').insert([
-        {id: 1, name: 'The Best Fairytale Ever', sentence: 'Once apon a time '}
+        {id: 1, name: 'Start Story', sentence: 'Once apon a time '}
       ])
     })
 }
--- seeds/storyNames.js
+++ seeds/storyNames.js
@@ -2,7 +2,7 @@ exports.seed = function (knex, Promise) {
   return knex('storyNames').del()
     .then(function () {
       return knex('storyNames').insert([
-        {id: 1, storyName: 'The Best Fairytale Ever'}
+        {id: 1, storyName: 'Start Story'}
       ])
     })
 }
